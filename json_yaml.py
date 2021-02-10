f4a49872d52a3bdeec680e1cd43a47a402794946
Kazuma Hoda <saiteigundan@gmail.com>
2021-02-11 00:07:15 +0900

    ファイル名を書き込めば相互変換できるところまで作成

--- json_yaml.py
+++ json_yaml.py
@@ -1,7 +1,12 @@
 import json
+import logging
+import logging.config
 import os
 
 from ruamel.yaml import YAML
+
+logging.config.fileConfig('logging.conf')
+logger = logging.getLogger(__name__)
 
 
 def yaml_init():
@@ -12,18 +17,47 @@ def yaml_init():
     return yaml
 
 
+def get_filename(path):
+    return os.path.splitext(os.path.basename(path))
+
+
 def json_to_yaml(path):
-    with open(path, mode='r', encoding='utf-8') as in_f:
-        d = json.load(in_f)
-    file_name = os.path.splitext(os.path.basename(path))[0]
     yaml = yaml_init()
-    with open(f'{file_name}.yml', mode='w', encoding='utf-8') as out_f:
-        yaml.dump(d, out_f)
+    try:
+        with open(path, mode='r', encoding='utf-8') as in_f:
+            d = json.load(in_f)
+        with open(f'{get_filename(path)[0]}.yml', mode='w', encoding='utf-8') as out_f:
+            yaml.dump(d, out_f)
+        logger.info('Parse YAML succeeded.')
+    except FileNotFoundError:
+        import traceback
+        logger.error(traceback.format_exc())
 
 
 def yaml_to_json(path):
-    pass
+    yaml = yaml_init()
+    try:
+        with open(path, mode='r', encoding='utf-8') as in_f:
+            d = yaml.load(in_f)
+        with open(f'{get_filename(path[0])}.json', mode='w', encoding='utf-8') as out_f:
+            json.dump(d, out_f, indent=2, ensure_ascii=False)
+        logger.info('Parse JSON succeeded.')
+    except FileNotFoundError:
+        import traceback
+        logger.error(traceback.format_exc())
 
+
+def main(path):
+    suffix = get_filename(path)[-1]
+    if suffix == '.yaml' or suffix == '.yml':
+        logger.info('Input file is YAML.')
+        yaml_to_json(path)
+    elif suffix == '.json':
+        logger.info('Input file is JSON.')
+        json_to_yaml(path)
+    else:
+        logger.warning('Input file is not YAML either JSON.')
+        logger.warning('Nothing to do.')
 
 if __name__ == '__main__':
-    json_to_yaml('./hoge.json')
+    main('./hoge.json')
